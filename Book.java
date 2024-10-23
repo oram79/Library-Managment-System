7b8aeaffa2e97bd2c2489ff92d2ad7092d993ade
Logan Oram <logan.oram@keyin.com>
2024-10-23 19:00:59 -0230

    book.java changed
    getters and setters were added as well as formatting for displaying each book

--- Book.java
+++ Book.java
@@ -1,13 +1,27 @@
-public class Book extends LibraryItem {
-    private String format;
+// Book.java
 
-    public Book(String id, String title, String author, String isbn, String publisher, int numOfCopies, String format)
-    {
-        super(id, title, author, isbn, publisher, numOfCopies);
+public class Book extends LibraryItem {
+    private String format; // Printed, Electronic, Audio
+
+    // Constructor
+    public Book(String id, String title, String author, String isbn, String publisher, int numberOfCopies, String format) {
+        super(id, title, author, isbn, publisher, numberOfCopies);  // Calls the constructor of LibraryItem
         this.format = format;
     }
-    
+
+    // Getter and Setter for format
+    public String getFormat() { return format; }
+    public void setFormat(String format) { this.format = format; }
+
+    @Override
+    public String toString() {
+    return "Book Title: " + this.getTitle() + ", Author: " + this.getAuthor() + ", ISBN: " + this.getIsbn();
+}
+
+    @Override
     public void displayDetails() {
-        System.out.println("Book [Title:] " + this.getTitle() + ", Author:" + this.author() + ", Format: " + this.format + "]");
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayDetails'");
     }
+
 }
