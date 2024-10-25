79ac38dd1691a122ac0da77f33ba07d398a1a855
Logan Oram <logan.oram@keyin.com>
2024-10-25 15:45:49 -0230

    employee class done

---
+++ Employee.java
@@ -0,0 +1,17 @@
+// Employee.java
+
+public class Employee extends Patron {
+    private String employeeID;
+
+    // Constructor
+    public Employee(String name, String address, String phoneNumber, String employeeID) {
+        super(name, address, phoneNumber);
+        this.employeeID = employeeID;
+    }
+
+    @Override
+    public void displayPatronDetails() {
+        System.out.println("Employee [Name: " + this.getName() + ", Employee ID: " + employeeID + "]");
+    }
+}
+
