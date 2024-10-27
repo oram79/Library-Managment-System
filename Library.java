e688b684f188c60018145e0761ac26bb800bf323
Logan Oram <logan.oram@keyin.com>
2024-10-27 15:40:03 -0230

    Library class done

--- Library.java
+++ Library.java
@@ -34,3 +34,41 @@ public class Library {
         }
         System.out.println("Item Not Available");
     }
+
+    // Method to get a patron by name
+    public Patron getPatronByName(String name) {
+        for (Patron patron : patrons) {
+            if (patron.getName().equalsIgnoreCase(name)) {
+                return patron;
+            }
+        }
+        return null; // Return null if no patron with the given name is found
+    }
+
+    // Method to return an item
+    public void returnItem(String title, Patron patron) {
+        for (LibraryItem item : items) {
+            if (item.getTitle().equalsIgnoreCase(title)) {
+                item.setNumberOfCopies(item.getNumberOfCopies() + 1); // Increment the number of copies
+                System.out.println(title + " has been returned by " + patron.getName());
+                return;
+            }
+        }
+        System.out.println("Item not found");
+    }
+
+    // Method to display all library items
+    public void displayAllItems() {
+        for (LibraryItem item : items) {
+            System.out.println(item);
+        }
+    }
+
+    // Method to display all patrons
+    public void displayAllPatrons() {
+        for (Patron patron : patrons) {
+            System.out.println(patron);
+        }
+    }
+}
+
