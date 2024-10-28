8a4a0ef8f1c2cc75953f7cfce0dad136f4c46456
Logan Oram <logan.oram@keyin.com>
2024-10-28 12:21:39 -0230

    code clean up

--- Student.java
+++ Student.java
@@ -1,21 +1,20 @@
-// Student.java
+// Student.java //
+
 public class Student extends Patron {
     private String studentID;
 
-    // Constructor
+    // Constructor //
     public Student(String name, String address, String phoneNumber, String studentID) {
         super(name, address, phoneNumber);
         this.studentID = studentID;
     }
 
-    @Override
     public String toString() {
     return "Student Name: " + this.getName() + ", ID: " + this.studentID + ", Address: " + this.getAddress() + ", Phone Number: " + this.getPhoneNumber();
 }
 
     @Override
     public void displayPatronDetails() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'displayPatronDetails'");
     }
 
